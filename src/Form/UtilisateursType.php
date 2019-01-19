cd2847c21ed88723a7d38363118be04ccd1f94eb
test <moumouni@yesbo.bf>
2019-01-19 15:12:00 +0000

    etat de la bceao

--- src/Form/UtilisateursType.php
+++ src/Form/UtilisateursType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,6 +20,9 @@ class UtilisateursType extends AbstractType
             ->add('prenom')
             ->add('estCaissier')
             //->add('status')
+            ->add('role', ChoiceType::class
+                ,array('choices'  => ['GUICHETIER'=>Utilisateurs::ROLE_GUICHETIER,'COMPTABLE'=>Utilisateurs::ROLE_COMPTABLE, 'ADMIN'=>Utilisateurs::ROLE_ADMIN], 'required' => true, 'mapped' =>false
+                ))
             ->add('compte')
         ;
     }
