b591e8f6e46ef1e5e04eb7207c6532184eea8e8c
Hamado OUEDRAOGO <ouedhams@yahoo.fr>
2019-01-30 08:42:18 +0000

    Menu et correction controller

--- src/Migrations/Version20190128181627.php
+++ src/Migrations/Version20190128181627.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190129183955 extends AbstractMigration
+final class Version20190128181627 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Devises ADD tx_reference DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE Caisses ADD dispo_guichet TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20190129183955 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Devises DROP tx_reference');
+        $this->addSql('ALTER TABLE Caisses DROP dispo_guichet');
     }
 }
