f7a1c3534b5f84dd8798bfb8978024c1545a9a4c
Hamado OUEDRAOGO <ouedhams@yahoo.fr>
2019-02-01 19:27:56 +0000

    correct pagination

--- src/Controller/ComptesController.php
+++ src/Controller/ComptesController.php
@@ -23,7 +23,7 @@ class ComptesController extends Controller
     {
         $limit=20;
         $_page=$request->query->get('_page');
-        $classe=$request->query->get('classe');
+        $classe=$request->query->get('master');
         //$offset = ($request)?$request->request->get('_page')*10:0;
         $offset = ($_page)?($_page-1)*$limit:0;
         $comptes = $this->getDoctrine()
